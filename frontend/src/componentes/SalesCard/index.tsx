5f6fa2188c06e426566e471a4c873444ced70387
Lucas Freitas <lucs.fa30@gmail.com>
2022-07-15 23:43:30 -0300

    DatePicker

--- frontend/src/componentes/SalesCard/index.tsx
+++ frontend/src/componentes/SalesCard/index.tsx
@@ -1,3 +1,6 @@
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+
 import NotificationButton from '../botaodenotificacao'
 import './styles.css'
 
@@ -7,10 +10,20 @@ function SalesCard() {
             <h2 className="dsmeta-sales-title">Vendas</h2>
             <div>
                 <div className="dsmeta-form-control-container">
-                    <input className="dsmeta-form-control" type="text"/>
+                    <DatePicker
+                        selected={new Date()}
+                        onChange={(date: Date) => { }}
+                        className="dsmeta-form-control"
+                        dateFormat="dd/MM/yyyy"
+                    />
                 </div>
                 <div className="dsmeta-form-control-container">
-                    <input className="dsmeta-form-control" type="text"/>
+                    <DatePicker
+                        selected={new Date()}
+                        onChange={(date: Date) => { }}
+                        className="dsmeta-form-control"
+                        dateFormat="dd/MM/yyyy"
+                    />
                 </div>
             </div>
 
@@ -38,8 +51,8 @@ function SalesCard() {
                             <td>
                                 <div className="dsmeta-red-btn-container">
                                     <div className="dsmeta-red-btn">
-                                          <NotificationButton />
-                                </div>
+                                        <NotificationButton />
+                                    </div>
                                 </div>
                             </td>
                         </tr>
@@ -52,9 +65,9 @@ function SalesCard() {
                             <td>R$ 55300.00</td>
                             <td>
                                 <div className="dsmeta-red-btn-container">
-                                <div className="dsmeta-red-btn-container">
-                                    <NotificationButton />
-                                </div>
+                                    <div className="dsmeta-red-btn-container">
+                                        <NotificationButton />
+                                    </div>
                                 </div>
                             </td>
                         </tr>
